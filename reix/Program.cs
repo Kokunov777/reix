68935c3e62b750050e3ffe289a8edda93bcfac5d
grim2281 <dym.09@bk.ru>
2024-11-16 17:30:27 +0700

    Добавьте файлы проекта.

---
+++ reix/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hel min");
+            Console.WriteLine("mid gard");
+        }
+    }
+}
